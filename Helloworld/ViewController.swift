f160a4fda1a0c76ebfbdf0fb63ded2aad3875e8a
aosandro10 <aoyama@ais-corp.co.jp>
2019-06-08 13:35:39 +0900

    Initial Commit

---
+++ Helloworld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Helloworld
+//
+//  Created by Shota Aoyama on 2019/06/08.
+//  Copyright © 2019 aosandro10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
